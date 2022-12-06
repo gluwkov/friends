f5be0962b2ebf0cd5a78ae3e395452312ccdece1
Ninja6228 <lesha6228nazarov@gmail.com>
2022-12-06 15:44:52 +0300

    исправлена кнопка, добавлен скролл

--- scripts/slider.js
+++ scripts/slider.js
@@ -38,36 +38,36 @@ $(document).ready(function () {
 			}
 		]
 	});
-});
 
-// слайдер блока promo, friends
-$('.slider').slick({
-	adaptiveHeight: true,
-	infinite: true,
-	slidesToShow: 1,
-	speed: 900,
-	autoplay: true,
-	autoplaySpeed: 11000,
-	dots: true,
-	fade: true,
-	dotsClass: 'slick-dots',
-	prevArrow: '<button type="button" class="slick-prev"><img src="../images/button/arrow-l.svg"></button>',
-	nextArrow: '<button type="button" class="slick-next"><img src="../images/button/arrow-r.svg"></button>',
-	responsive: [
-		{
-			breakpoint: 850,
-			settings: {
-				arrows: false,
-				fade: false,
-			}
-		},
-	]
+	// слайдер блока promo, friends
+	$('.slider').slick({
+		adaptiveHeight: true,
+		infinite: true,
+		slidesToShow: 1,
+		speed: 900,
+		autoplay: true,
+		autoplaySpeed: 11000,
+		dots: true,
+		fade: true,
+		dotsClass: 'slick-dots',
+		prevArrow: '<button type="button" class="slick-prev"><img src="../images/button/arrow-l.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="../images/button/arrow-r.svg"></button>',
+		responsive: [
+			{
+				breakpoint: 850,
+				settings: {
+					arrows: false,
+					fade: false,
+				}
+			},
+		]
+	});
 });
 
 // слайдер блока blog
-$(window).on('load resize', function() {
-  if ($(window).width() < 769) {
-    $('.blog__cards:not(.slick-initialized)').slick({
+$(window).on('load resize', function () {
+	if ($(window).width() < 769) {
+		$('.blog__cards:not(.slick-initialized)').slick({
 			infinite: false,
 			adaptiveHeight: true,
 			speed: 1100,
@@ -77,15 +77,18 @@ $(window).on('load resize', function() {
 			arrows: false,
 			dots: true,
 			dotsClass: 'slick-dots',
-    });
-  } else {
-    $(".blog__cards.slick-initialized").slick("unslick");
-  }
+		});
+	} else {
+		$(".blog__cards.slick-initialized").slick("unslick");
+	}
 });
 
-// // скрол для кнопки из слайдера в блок programs
-// $("a[href='#scroll-programs']").click(function () {
-//   const _href = $(this).attr("href");
-//   $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
-//   return false;
-// });
+// скрол для кнопки из слайдера в блок programs
+$(function () {
+	$("a[href='#scroll-programs']").click(function () {
+		const _href = $(this).attr("href");
+		$("html, body").animate({ scrollTop: $(_href).offset().top }, 1500);
+		return false;
+	});
+});
+
