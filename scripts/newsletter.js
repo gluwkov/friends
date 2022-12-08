e8f6d2de874cc2e18e9b38ed1e2e706ecde0686a
Daniil Glushkov <acize@yandex.ru>
2022-12-08 14:40:15 +0300

    feat: Добавить fontAwesome в newsletter

--- scripts/newsletter.js
+++ scripts/newsletter.js
@@ -1,18 +1,23 @@
-let emailInput = document.querySelector('.newsletter__input');
-let clearButton = document.querySelector('.newsletter__clearButton');
+const emailInput = document.querySelector('.newsletter__input');
+const clearButton = document.querySelector('.newsletter__clearButton');
+const placeholder = document.querySelector('.newsletter__placeholder-container');
 
-emailInput.addEventListener("input", hideClearButton);
+emailInput.addEventListener('input', hideItems);
+placeholder.addEventListener('input', hideItems);
 clearButton.addEventListener('click', clearInput);
 
-function hideClearButton() {
+function hideItems() {
 	if (emailInput.value) {
 		clearButton.classList.remove('newsletter__clearButton_hidden');
+		placeholder.classList.add('newsletter__placeholder-container_hidden');
 	} else {
 		clearButton.classList.add('newsletter__clearButton_hidden');
+		placeholder.classList.remove('newsletter__placeholder-container_hidden');
 	}
 }
 
 function clearInput() {
 	emailInput.value = '';
 	clearButton.classList.add('newsletter__clearButton_hidden');
+	placeholder.classList.remove('newsletter__placeholder-container_hidden');
 }
